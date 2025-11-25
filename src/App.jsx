1b9e1ae0de45947fe5a8b752f99f1e39cf41d43b
sullaspqr <sulla.spqr78@gmail.com>
2025-11-25 11:27:33 +0100

    első kommit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+const ListaKomponens = ({ elemek }) => (
+  <ol>
+    {elemek.map((elem, index) => (
+      <li key={index}>{elem.name}-{elem.email}<br/><br/>
+      {elem.body}</li>
+    ))}
+  </ol>
+);
+export const App =() => {
+  const [adatok, setAdatok] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:5143/Comment')
+    .then((res) => (res.ok ? res.json(): []))
+    .then((tartalom) => setAdatok(tartalom));
+  }, []) // itt van a dependency array
+
+  return (
+    <div className="container">
+     <div className="row m-5 p-5 border">
+        <ListaKomponens elemek={adatok} />
+     </div>
+    </div>
+  )
+}
+
+export default App;
